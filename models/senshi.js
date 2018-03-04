ca58a12735c17ef4be1f8738613042aef42c0d8f
ixnp <enprocopios@gmail.com>
2018-03-03 17:58:30 -0800

    mod build with jb

---
+++ models/senshi.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const uuidv4 = require('uuidv4');
+  class Senshi{
+    constructor(senshi,name,power){
+        this.id= uuidv4();
+        this.senshi = senshi;
+        this.name = name;
+        this.power = power;
+
+    }
+}
+ 
+module.exports = Senshi;
